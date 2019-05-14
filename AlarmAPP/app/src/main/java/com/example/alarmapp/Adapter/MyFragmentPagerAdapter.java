68ed42eed4317e5bb60f0ae34f178bc45ef517fe
xiaoxin83121 <xiaoxin83121@gmail.com>
2019-05-14 16:24:52 +0800

    manu merge frontend into this repo

---
+++ AlarmAPP/app/src/main/java/com/example/alarmapp/Adapter/MyFragmentPagerAdapter.java
@@ -0,0 +1,60 @@
+package com.example.alarmapp.Adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.view.ViewGroup;
+
+import com.example.alarmapp.Fragment.FragmentHome;
+import com.example.alarmapp.Fragment.FragmentNotification;
+import com.example.alarmapp.Fragment.FragmentSettings;
+import com.example.alarmapp.MainActivity;
+
+public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    private final int PAGER_COUNT = 3;
+    private FragmentHome fragmentHome = null;
+    private FragmentNotification fragmentNotification = null;
+    private FragmentSettings fragmentSettings = null;
+
+    public MyFragmentPagerAdapter(FragmentManager fm){
+        super(fm);
+        fragmentHome = new FragmentHome();
+        fragmentNotification = new FragmentNotification();
+        fragmentSettings = new FragmentSettings();
+    }
+
+    @Override
+    public int getCount(){
+        return PAGER_COUNT;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup vg, int position) {
+        return super.instantiateItem(vg, position);
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        System.out.println("position Destory" + position);
+        super.destroyItem(container, position, object);
+    }
+
+    @Override
+    public Fragment getItem(int position){
+        Fragment fragment = null;
+        switch (position){
+            case MainActivity.PAGE_HOME:
+                fragment = fragmentHome;
+                break;
+            case MainActivity.PAGE_NOTIFICATION:
+                fragment = fragmentNotification;
+                break;
+            case MainActivity.PAGE_SETTINGS:
+                fragment = fragmentSettings;
+                break;
+        }
+        return fragment;
+    }
+
+}
---
+++ AlarmAPP/app/src/main/java/com/example/alarmapp/Fragment/FragmentNotification.java
@@ -0,0 +1,123 @@
+package com.example.alarmapp.Fragment;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.alarmapp.Adapter.MyRecycleViewAdapter;
+import com.example.alarmapp.R;
+import com.example.alarmapp.Utils.AlarmInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FragmentNotification extends Fragment {
+    private Button button = null;
+    private DatePicker datePicker = null;
+    private TextView date_text = null;
+    private String text;
+    private RecyclerView card_list;
+    private MyRecycleViewAdapter myRecycleViewAdapter;
+    private List<AlarmInfo> alarmInfoList = new ArrayList<>();
+
+    public FragmentNotification(){
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fg_notification, container, false);
+        Log.i("Fragment2", "notification");
+        button = view.findViewById(R.id.select_date_button);
+        date_text = view.findViewById(R.id.date_text);
+        card_list = view.findViewById(R.id.card_list);
+        card_list.setHasFixedSize(true);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        card_list.setLayoutManager(layoutManager);
+        generate_test_date();
+        myRecycleViewAdapter = new MyRecycleViewAdapter(alarmInfoList);
+        card_list.setAdapter(myRecycleViewAdapter);
+//        card_list.setNestedScrollingEnabled(false);
+        initEvent();
+        return view;
+    }
+
+    private void generate_test_date(){
+        AlarmInfo a1 = new AlarmInfo("1");
+        alarmInfoList.add(a1);
+        AlarmInfo a2 = new AlarmInfo("2");
+        alarmInfoList.add(a2);
+        AlarmInfo a3 = new AlarmInfo("3");
+        alarmInfoList.add(a3);
+        AlarmInfo a4 = new AlarmInfo("4");
+        alarmInfoList.add(a4);
+        AlarmInfo a5 = new AlarmInfo("5");
+        alarmInfoList.add(a5);
+        AlarmInfo a6 = new AlarmInfo("6");
+        alarmInfoList.add(a6);
+        AlarmInfo a7 = new AlarmInfo("7");
+        alarmInfoList.add(a7);
+        AlarmInfo a8 = new AlarmInfo("8");
+        alarmInfoList.add(a8);
+        AlarmInfo a9 = new AlarmInfo("9");
+        alarmInfoList.add(a9);
+
+    }
+
+    private void initEvent(){
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                showDialog();
+            }
+        });
+    }
+
+    private void showDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.datepicker_layout, null);
+        builder.setView(view);
+        /*
+        * 这里一定要搞清楚，datepicker是在弹出dialog之后才加载，所以获取datepicker对象也要在其存在的情况下，】
+        * 否则会出现NullPointerException
+        * */
+        datePicker = view.findViewById(R.id.date_picker);
+        datePicker.init(2019, 5, 9, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker datePicker, int i, int i1, int i2) {
+                text = "Year:"+i+" Month:"+i1+" Day:"+i2 ;
+            }
+        });
+
+        builder.setPositiveButton(R.string.confirm, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //设置时间
+                Toast.makeText(getContext(), "xiajiba", Toast.LENGTH_LONG).show();
+                date_text.setText(text);
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //取消
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+}
